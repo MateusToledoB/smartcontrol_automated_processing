8ef631a3dcc5bc7ac7dc11b2130af583cc4c313e
Mateus Toledo Benkenstein <mateus.benkenstein@Galaxia.local>
2026-05-08 16:31:55 -0300

    colocando temporarios ao start boot

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-echo "Iniciando worker_problema..."
+echo "Iniciando worker_problema_no_equipamento..."
 python interfaces/workers/problema_no_equipamento.py &
 
 echo "Iniciando worker_apontamento_impar..."
 python interfaces/workers/apontamento_impar.py &
+
+echo "Iniciando worker_temporarios..."
+python interfaces/workers/temporarios.py &
 
 wait
